9198ade13241473dbcbee0ebde3eca985cda19f0
Grant Novota <47872713+Novota15@users.noreply.github.com>
2021-12-13 17:28:23 -0700

    add assignment4 core affinity

---
+++ assignment04/pthread.c
@@ -0,0 +1,140 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sched.h>
+#include <syslog.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+
+// multi-threading with core affinity
+
+// notes for the assignment:
+// before printing to syslog clear it with:
+// echo > /dev/null | sudo tee /var/log/syslog 
+
+// for the first line you need to print uname -a, use logger command on the command line
+// logger [COURSE:X][ASSIGNMENT:Y]: `uname -a`
+
+// #define _GNU_SOURCE // to avoid get_cpu() implicit declaration warning
+
+#define NUM_THREADS 128
+#define SCHED_POLICY SCHED_FIFO
+
+typedef struct {
+    int threadIdx;
+} threadParams_t;
+
+// POSIX thread declarations and scheduling attributes
+pthread_t threads[NUM_THREADS];
+threadParams_t threadParams[NUM_THREADS];
+
+pthread_attr_t fifo_sched_attr;
+struct sched_param fifo_param;
+
+// print scheduling policy
+void print_scheduler(void) {
+    int sched_type = sched_getscheduler(getpid());
+    char *sched = malloc(256);
+    
+    switch (sched_type)
+    {
+    case SCHED_FIFO:
+        strcpy(sched, "SCHED_FIFO");
+        break;
+    case SCHED_OTHER:
+        strcpy(sched, "SCHED_OTHER");
+        break;
+    default:
+        strcpy(sched, "UNKNOWN");
+        break;
+    }
+
+    printf("Pthread policy is: %s\n", sched);
+    free(sched);
+}
+
+// set up scheduler
+void set_scheduler(void) {
+    int max_priority; // priority values are 1-99 by default
+    int rc;
+    int cpuidx;
+    cpu_set_t cpuset;
+
+    printf("Before policy adjustment:\n");
+    print_scheduler();
+
+    pthread_attr_init(&fifo_sched_attr);
+    pthread_attr_setinheritsched(&fifo_sched_attr, PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&fifo_sched_attr, SCHED_POLICY);
+
+    // set up CPU affinity
+    CPU_ZERO(&cpuset);
+    cpuidx=(3);
+    CPU_SET(cpuidx, &cpuset);
+    pthread_attr_setaffinity_np(&fifo_sched_attr, sizeof(cpu_set_t), &cpuset);
+
+    max_priority = sched_get_priority_max(SCHED_POLICY);
+    fifo_param.sched_priority = max_priority;
+
+    if ((rc = sched_setscheduler(getpid(), SCHED_POLICY, &fifo_param)) < 0) {
+        perror("sched_setscheduler");
+	}
+    
+    pthread_attr_setschedparam(&fifo_sched_attr, &fifo_param);
+
+    printf("After policy adjustment:\n");
+    print_scheduler();
+}
+
+void *counter_thread(void *threadp) {
+    int sum = 0, i;
+    threadParams_t *threadParams = (threadParams_t *)threadp;
+
+    for(i = 1; i <= (threadParams->threadIdx); i++)
+        sum=sum+i;
+ 
+    syslog(LOG_DEBUG, "Thread idx=%d, sum[0...%d]=%d, running on core:%d\n", 
+           threadParams->threadIdx,
+           threadParams->threadIdx, sum, sched_getcpu());
+}
+
+void delay(unsigned int mseconds) {
+    clock_t goal = mseconds + clock();
+    while (goal > clock());
+}
+
+int main (int argc, char *argv[]) {
+    // clear syslog
+    system("echo > /dev/null | tee /var/log/syslog");
+    // create first line
+    system("logger [COURSE:1][ASSIGNMENT:4]: `uname -a` | tee /var/log/syslog");
+    // open for logging
+    openlog("[COURSE:1][ASSIGNMENT:4]", LOG_NDELAY, LOG_DAEMON);
+
+    set_scheduler();
+    
+    for(int i = 0; i < NUM_THREADS; i++) {
+        threadParams[i].threadIdx=i;
+
+        pthread_create(&threads[i],   // pointer to thread descriptor
+                      &fifo_sched_attr,     // use fifo
+                      counter_thread, // thread function entry point
+                      (void *)&(threadParams[i]) // parameters to pass in
+                     );
+   } 
+
+    for(int i = 0; i < NUM_THREADS; i++)
+        pthread_join(threads[i], NULL);
+    
+    closelog();
+
+    // move syslog output to text file for submission
+    // fclose(fopen("assignment2.txt", "w"));
+    // system("tail -n 129 /var/log/syslog > assignment2.txt");
+    delay(100000);
+    system("cp /var/log/syslog assignment4.txt");
+
+    printf("\nComplete\n");
+    return 0;
+}
